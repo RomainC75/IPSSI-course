db4dc64b15b9701198850dca0112ab13fb20c37a
RomainC75 <rom.chenard@gmail.com>
2022-12-07 11:29:51 +0100

    d2/ex4

---
+++ JS/D2/ex4.js
@@ -0,0 +1,19 @@
+const avg = (array) =>{
+    let sum = 0
+    array.forEach(element => {
+        console.log(element, typeof element)
+        if(typeof element === "number"){
+            sum+=element
+        }else if(typeof element === "string"){
+            sum+=element.length
+        }else if(typeof element === "boolean" && element){
+            sum+=1
+        }
+    });
+    return sum/array.length
+}
+
+const mixedArr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10];
+const average = avg(mixedArr)
+// should return: 5.7
+console.log(average)
